9fffab3f90ae370d6bc117389994d1a731fd5d93
Thong Khong <thongtv2006@gmail.com>
2025-10-05 23:58:00 -0400

    Delete MainWindowViewModel.cs

--- Flight Advisor/ViewModels/MainWindowViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FlightAdvisor.ViewModels
-{
-    public partial class MainWindowViewModel : ViewModelBase
-    {
-        public string Greeting { get; } = "Welcome to Avalonia!";
-    }
-}
