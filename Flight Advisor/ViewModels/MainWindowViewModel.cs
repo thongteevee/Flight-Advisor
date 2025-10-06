a838007eef594e9195baf31ad2a841c3bff5fddd
Thong Khong <thongtv2006@gmail.com>
2025-10-05 23:22:04 -0400

    Add project files.

---
+++ Flight Advisor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Flight_Advisor.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ Flight Advisor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Flight_Advisor.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
